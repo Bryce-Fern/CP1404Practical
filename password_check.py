65a14031867c89e2ca837b6757924c51533bb32a
Bryce Fernq <bryce.fern@my.jcu.edu.au>
2020-03-15 23:35:43 +1100

    Refactor password check, now with 100% more functions

--- password_check.py
+++ password_check.py
@@ -1,7 +1,22 @@
 length_password = 4
 
-password = input("Enter a password with {} or more characters: ".format(length_password))
-while len(password) < length_password:
-    password = input("Invalid password, password must have at least {} characters: ".format(length_password))
-else:
+def main():
+    password = get_password()
+    while len(password) < length_password:
+        password = input("Invalid password, password must have at least {} characters: ".format(length_password))
+    else:
+        asterisks(password)
+
+
+def asterisks(password):
     print('*' * len(password))
+
+
+def get_password():
+    password = input("Enter a password with {} or more characters: ".format(length_password))
+    return password
+
+
+main()
+
+
