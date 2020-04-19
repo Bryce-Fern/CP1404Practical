012b8611b413e8695be2002fd622c49e7a6e3f7f
Bryce-Fern <62205907+Bryce-Fern@users.noreply.github.com>
2020-04-19 23:09:30 +1000

    Delete hex_colours.py

--- hex_colours.py
+++
@@ -1,9 +0,0 @@
-colour_format = {"AliceBlue": "#f0f8ff", "aquamarine1": "#7fffd4", "purple": "#a020f0", "red1": "#ff0000", "DeepPink1": "#ff1493", "turquoise1": "#00f5ff", "SpringGreen1": "#00ff7f", "magenta": "ff00ff", "cyan1": "#00ffff", "DarkGoldenrod1": "#ffb90f", "burlywood": "#deb887", "orchid1": "#ff83fa"}
-
-colour_hex = input("Enter colour: ")
-while colour_hex != "":
-    if colour_hex in colour_format:
-        print(colour_hex, "hex code is", colour_format[colour_hex])
-    else:
-        print("Invalid Hex Colour")
-    colour_hex = input("Enter colour: ")
